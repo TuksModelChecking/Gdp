2386d9721a467eabe2deb8381a3bfd83c54d6502
Siocnarff <josuabotha@protonmail.com>
2021-02-16 16:35:20 +0200

    create ispl outline from gdp.yaml file using TabbedWriter object

--- src/main.py
+++ src/main.py
@@ -12,20 +12,13 @@ class TabbedWriter:
         tabs = ("\t" * self.tab_depth)
         self.file.write(f"{tabs}{str}\n")
 
-    def write_tab(self, str):
-        self.write(str)
+    def write_tab(self, string):
+        self.write(string)
         self.tab_depth += 1
 
-    def untab_write(self, str):
+    def untab_write(self, string):
         self.tab_depth -= 1
-        self.write(str)
-
-    def tab(self):
-        self.tab_depth += 1
-
-    def untab(self):
-        self.tab_depth -= 1
-
+        self.write(string)
 
 
 with open("gdp.yaml", "r") as gdp_yaml:
@@ -33,28 +26,39 @@ with open("gdp.yaml", "r") as gdp_yaml:
 
 file = open("out.ispl", "w")
 tw = TabbedWriter(file)
+num_agents = len(gdp["agents"])
 
-
-tw.write("Semantics=SingleAssignment;")
+tw.write("Semantics=SingleAssignment;\n")
 
 tw.write_tab("Agent Environment")
-tw.write_tab("Vars")
+tw.write_tab("Vars:")
+for v in gdp["resources"]:
+    tw.write(f"{v}: 0..{num_agents};")
 tw.untab_write("end Vars")
-tw.untab_write("end Agent")
+tw.untab_write("end Agent\n")
 
 for a in gdp["agents"]:
     tw.write_tab(f"Agent {a}")
-    tw.write("...")
-    tw.untab_write(f"end {a}")
+    lobsvars = "{"
+    for v in gdp[a]["access"]:
+        lobsvars += v
+        lobsvars += ","
+    lobsvars = lobsvars[:-1] + "};"
+    tw.write(f"Lobsvars = {lobsvars}")
+    tw.write_tab("Vars:")
+    demand = gdp[a]["demand"]
+    tw.write(f"rem: 0..{demand};")
+    tw.untab_write("end Vars")
+    tw.untab_write(f"end Agent\n")
 
 tw.write_tab("Evaluation")
-tw.untab_write("end Evaluation")
+tw.untab_write("end Evaluation\n")
 
 tw.write_tab("InitStates")
-tw.untab_write("end InitStates")
+tw.untab_write("end InitStates\n")
 
 tw.write_tab("Groups")
-tw.untab_write("end Groups")
+tw.untab_write("end Groups\n")
 
 tw.write_tab("Formulae")
-tw.untab_write("end Formulae")
+tw.untab_write("end Formulae\n")
