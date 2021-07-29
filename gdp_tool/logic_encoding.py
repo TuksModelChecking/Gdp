f3f81daf0b49da1e54e6b380c217230785c0d824
Josua Botha <josua.public@gmail.com>
2021-07-29 15:51:28 +0200

    get a working example of conjunction over the symbols for def 12

--- gdp_tool/logic_encoding.py
+++ gdp_tool/logic_encoding.py
@@ -1,13 +1,24 @@
+import math
+
 from yaml import load
 from yaml import SafeLoader
+from sympy import *
 from pysat.formula import CNF
-from pysat.solvers import Lingeling
+from pysat.solvers import Minisat22
 
 # Let "Paper" be used to denote the SMBF2021 submission by Nils Timm and Josua Botha
 
 problem = load(open("input.yml", "r"), Loader=SafeLoader)
 
+
 # By Definition 12 in Paper
-def encode_resource_state(resource: int, agent: int, time: int) -> CNF:
-    encoding = CNF()
-    return encoding
+def encode_resource_state(resource: int, agent: int, time: int, num_agents: int) -> CNF:
+    m: int = math.ceil(math.log(num_agents, 2))
+    symbol_assembly_line = []
+    for b in reversed(range(0, m)):
+        symbol_assembly_line.append(f"r{resource}a{agent}t{time}b{b}")
+    boolean_symbols = symbols(symbol_assembly_line)
+    return to_cnf(And(*boolean_symbols))
+
+
+print(encode_resource_state(1, 1, 1, 8))
