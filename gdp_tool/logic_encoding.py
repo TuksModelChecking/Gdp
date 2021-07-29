554dd2fea7e5df1e69aca5e36f11aa22cdf48b3f
Josua Botha <josua.public@gmail.com>
2021-07-29 16:16:48 +0200

    add ascii encode and decode of named variables

--- gdp_tool/logic_encoding.py
+++ gdp_tool/logic_encoding.py
@@ -1,4 +1,5 @@
 import math
+import re as r
 
 from yaml import load
 from yaml import SafeLoader
@@ -11,6 +12,24 @@ from pysat.solvers import Minisat22
 problem = load(open("input.yml", "r"), Loader=SafeLoader)
 
 
+# A helper function that can convert unique a named variable into a unique integer, created so that pysat can be used
+# alongside variables given non-integer names, as in the Paper, to make this program easier to map to the paper
+def ascii_encode(s: Symbol) -> int:
+    name = s.__str__()
+    ascii_encoding = ""
+    for char in name:
+        ascii_encoding += str(ord(char)).zfill(3)
+    return int(ascii_encoding)
+
+
+# Acts as the "antidote" to ascii_encode: given a number it spits out the original Symbol
+def ascii_decode(number: int) -> Symbol:
+    name = ""
+    for encoded_char in r.findall('...', str(number)):
+        name += chr(int(encoded_char))
+    return Symbol(name)
+
+
 # By Definition 12 in Paper
 def encode_resource_state(resource: int, agent: int, time: int, num_agents: int) -> CNF:
     m: int = math.ceil(math.log(num_agents, 2))
@@ -18,7 +37,12 @@ def encode_resource_state(resource: int, agent: int, time: int, num_agents: int)
     for b in reversed(range(0, m)):
         symbol_assembly_line.append(f"r{resource}a{agent}t{time}b{b}")
     boolean_symbols = symbols(symbol_assembly_line)
+    for sym in boolean_symbols:
+        print(ascii_decode(ascii_encode(sym)))
     return to_cnf(And(*boolean_symbols))
 
 
-print(encode_resource_state(1, 1, 1, 8))
+encode_resource_state(1, 1, 1, 8)
+
+# with Minisat22(bootstrap_with=encode_resource_state(1, 1, 1, 8)) as mini:
+#     print(mini.solve())
